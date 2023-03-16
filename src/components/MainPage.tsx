8d223e03def160c4e37062c84cd4a42612004b3e
VindexTOS <giorgikutateladze1998@gmail.com>
2023-03-16 23:42:10 +0400

    main img and deisgn

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import questions from '../data/data'
 import { UseQuestionContext } from './context'
-
+import questionMainImg from '../assets/questionMainPhoto.jpg'
 const MainPage = () => {
   const { randomQuestion } = UseQuestionContext()
-
+  const style = {
+    mainDiv: `flex items-center justify-center w-[100vw] h-[100vh] flex-col gap-10 `,
+    img: ` w-[70%] h-[500px] boxshaddow rounded-[5px]`,
+    btn: `w-[70%] h-[2.3rem] bg-[#3d9d9b] rounded-[2px] text-white font-bold hover:bg-[#348582]`,
+  }
   return (
-    <div className="flex items-center justify-center w-[100vw] h-[100vh] ">
-      <button
-        className="w-[9rem] h-[3rem] bg-blue-400 rounded-[30px] text-white font-bold hover:bg-blue-500"
-        onClick={randomQuestion}
-      >
+    <div className={style.mainDiv}>
+      <img className={style.img} src={questionMainImg} />
+      <p className="text-gray-400 text-[1.2rem] w-[70%] text-center">
+        Here, you can prepare for a front-end/React job interview by practicing
+        commonly asked questions. I have extensively researched this subject and
+        used multiple sources to gather the information.
+      </p>
+      <button className={style.btn} onClick={randomQuestion}>
         Start
       </button>
     </div>
